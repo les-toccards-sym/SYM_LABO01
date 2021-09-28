e28ea7857c9fbf61e1b1d4a86939e3e3566728c6
Doran Kayoumi <dorankayoumi@gmail.com>
2021-09-28 13:25:28 +0200

    creat new activity for registration

---
+++ app/src/main/java/ch/heigvd/iict/sym/labo1/RegisterActivity.kt
@@ -0,0 +1,11 @@
+package ch.heigvd.iict.sym.labo1
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class RegisterActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_register)
+    }
+}
